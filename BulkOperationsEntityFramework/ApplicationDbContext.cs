c385775f7f0fc2a3e1dc1ada4bb44fa6703e8e58
Tore Aurstad <tore.aurstad@gmail.com>
2025-05-31 22:58:42 +0200

    Init demo for Bulk operations in Entity Framework

---
+++ BulkOperationsEntityFramework/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using BulkOperationsEntityFramework.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+
+namespace BulkOperationsEntityFramework
+{
+
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext() : base("name=App")
+        {            
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasKey(u => u.Id);
+            modelBuilder.Entity<User>().Property(u => u.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+        }
+
+    }
+
+}
+
+
